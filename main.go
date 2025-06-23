372909c7433f1b517ce89049821895b1d9971cc3
Ivan <ivanmalaevkirova99@gmail.com>
2025-06-23 20:45:06 +0400

    Выполнена задача 2

--- main.go
+++ main.go
@@ -6,20 +6,30 @@ func main() {
 
 	// 1 задача
 
+	// var char1 int
+
+	// for char1 = 1; char1 <= 50; char1++ {
+
+	// 	if char1%3 == 0 && char1%5 == 0 {
+	// 		fmt.Println("FizzBuzz")
+	// 	} else if char1%3 == 0 {
+	// 		fmt.Println("Fizz")
+	// 	} else if char1%5 == 0 {
+	// 		fmt.Println("Buzz")
+	// 	} else {
+	// 		fmt.Println(char1)
+	// 	}
+
+	// }
+
+	// 2 задача
+
 	var char1 int
 
-	for char1 = 1; char1 <= 50; char1++ {
-
-		if char1%3 == 0 && char1%5 == 0 {
-			fmt.Println("FizzBuzz")
-		} else if char1%3 == 0 {
-			fmt.Println("Fizz")
-		} else if char1%5 == 0 {
-			fmt.Println("Buzz")
-		} else {
-			fmt.Println(char1)
-		}
-
+	summ := 0
+	for char1 = 1; char1 <= 100; char1++ {
+		summ += char1
 	}
+	fmt.Println(summ)
 
 }
